84ceee3d7473983a939af7cd50520236233e7153
Suprith Gowda <suprith.19gowda@gmail.com>
2026-06-05 11:14:12 +0530

    updated  export options

--- app/Exports/VibhagExport.php
+++ app/Exports/VibhagExport.php
@@ -22,18 +22,15 @@ class VibhagExport implements FromCollection , WithHeadings
     public function collection()
     {
         $list =  DB::table('customers')
-        ->select('customers.reg_id','customers.username','customers.phone','customers.email','customers.pin', 'customers.address','pwa_category.name as career', 'pwa_subcategory.name as careersub', 'customers.yod','pwa_responsibility.name as javb', 'pwa_responsibility_sub.name as javbviv', 'jm_blr_rs_vasathi.name as vname', 'jm_blr_rs_upavasathi.name as upavasathi_name', 'jm_blr_rs_nagar.name as nname', 'jm_blr_rs_bhag.name as bname', 'jm_blr_rs_vibhag.name as vibname','customers.created_at')
+        ->select('customers.reg_id','customers.username','customers.phone','customers.email','customers.pin', 'customers.address','pwa_category.name as career', 'pwa_subcategory.name as careersub', 'customers.yod','pwa_responsibility.name as javb', 'jm_blr_rs_vasathi.name as vname', 'jm_blr_rs_upavasathi.name as upavasathi_name', 'jm_blr_rs_nagar.name as nname', 'jm_blr_rs_bhag.name as bname', 'jm_blr_rs_vibhag.name as vibname','customers.created_at')
         ->leftJoin('jm_blr_rs_vasathi', 'customers.area', '=', 'jm_blr_rs_vasathi.id')
         ->leftJoin('jm_blr_rs_upavasathi', 'customers.upavasathi', '=', 'jm_blr_rs_upavasathi.id')
         ->leftJoin('jm_blr_rs_nagar', 'customers.taluk', '=', 'jm_blr_rs_nagar.id')
         ->leftJoin('jm_blr_rs_bhag', 'customers.district', '=', 'jm_blr_rs_bhag.id')
         ->leftJoin('jm_blr_rs_vibhag', 'customers.city', '=', 'jm_blr_rs_vibhag.id')
-        ->leftJoin('pwa_jagrithi', 'customers.jag', '=', 'pwa_jagrithi.id')
-        ->leftJoin('pwa_intre_area', 'customers.intrarea', '=', 'pwa_intre_area.id')
         ->leftJoin('pwa_category', 'customers.prof', '=', 'pwa_category.id')
         ->leftJoin('pwa_subcategory', 'customers.are', '=', 'pwa_subcategory.id')
         ->leftJoin('pwa_responsibility', 'customers.resp', '=', 'pwa_responsibility.id')
-        ->leftJoin('pwa_responsibility_sub', 'customers.resposub', '=', 'pwa_responsibility_sub.id')
         ->where('customers.city', $this->id)
         ->orderBy('customers.id', 'ASC')
         ->get();
@@ -44,6 +41,6 @@ class VibhagExport implements FromCollection , WithHeadings
     {
         return [
             'Registration id', 'Name', 'Phone', 'Email', 'Pincode', 'Address',
-            'Career', 'Career Sub', 'Year of Birth', 'Javabdari', 'Vividhakshetra', 'Vasathi', 'Upavasathi', 'Nagar', 'Bhag', 'Vibhag', 'Created_at'];
+            'Career', 'Career Sub', 'Year of Birth', 'Javabdari', 'Vasathi', 'Upavasathi', 'Nagar', 'Bhag', 'Vibhag', 'Created_at'];
         } 
     }
